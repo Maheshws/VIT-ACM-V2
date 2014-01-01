f85b0c5e95f01c3a8e8b8e75fa360bfe0c616b05
Mahesh <mahesh@mahesh.ws>
2014-01-01 23:01:34 +0530

    Preferences
    Preferences added only Activity

--- vitacm/src/main/java/com/mahesh/vitacm/PreferencesActivity.java
+++ vitacm/src/main/java/com/mahesh/vitacm/PreferencesActivity.java
@@ -2,11 +2,14 @@ package com.mahesh.vitacm;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Mahesh on 12/21/13.
@@ -17,6 +20,7 @@ public class PreferencesActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences_activity);
+
         SharedPreferences pref = getSharedPreferences("user_settings", 0); // 0 - for private mode
         editor = pref.edit();
         final CheckBoxPreference Notification = (CheckBoxPreference) getPreferenceManager().findPreference("Notification");
@@ -28,6 +32,24 @@ public class PreferencesActivity extends PreferenceActivity {
                 return true;
             }
         });
+
+        Preference CurrentVersion= findPreference("CurrentVersion");
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            CurrentVersion.setSummary("v"+versionName);
+            CurrentVersion.setTitle("Application Version");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        Preference ClearData= findPreference("ClearData");
+        ClearData.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getApplicationContext(),"In Next Update :P",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
        /*
         final CheckBoxPreference Theme = (CheckBoxPreference) getPreferenceManager().findPreference("Theme");
         Theme.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
